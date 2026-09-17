33d9d00a23298ac756d3c528c20f55e1ba020e9f
Bernadette Lambrecht <Lambrecht.Bernadette@proton.me>
2026-09-17 15:24:05 +0200

    Page Contact
    Appel de la page de contact

--- www/app/routers/index.php
+++ www/app/routers/index.php
@@ -1,29 +1,34 @@
 
 <?php
 
-
+// PAGE CONTACT
+// PATTERN: ?
+if (isset($_GET['contact'])) {
+    $title = "Contact";
+    ob_start();
+    include_once '../app/views/templates/partials/contact.php';
+    $content = ob_get_clean();
+}
 // ROUTES POSTS
 // PATTERN: /posts/...
 // URL: ?posts=...
 // ROUTER posts
-if (isset($_GET['posts'])):
+elseif (isset($_GET['posts'])) {
     include_once '../app/routers/posts.php';
-
-
+}
 // ROUTES USERS
 // PATTERN: /users/...
 // URL: ?users=...
 // ROUTER users
-elseif (isset($_GET['users'])):
+elseif (isset($_GET['users'])) {
     include_once '../app/routers/users.php';
-
-
+}
 // ROUTE PAR DÉFAUT: Les 10 derniers posts
 // PATTERN: /
 // URL: ?
 // CTRL: postsController
 // ACTION: index
-else:
+else {
     include_once '../app/controllers/postsController.php';
     \App\Controllers\PostsController\indexAction($connexion);
-endif;
+}
